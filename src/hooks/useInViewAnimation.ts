1288bd8e4f7e44590e2d976da91e40d098529617
Joshua <mojakokudasai@gmail.com>
2025-11-06 23:36:42 +0800

    Fix: App Building and Package issues

--- src/hooks/useInViewAnimation.ts
+++ src/hooks/useInViewAnimation.ts
@@ -1,10 +1,15 @@
 import { useAnimation, useInView } from 'framer-motion'
 import { useEffect, useRef } from 'react'
 
-export function useInViewAnimation(options?: IntersectionObserverInit) {
+export function useInViewAnimation(options?: { once?: boolean }) {
   const ref = useRef<HTMLElement | null>(null)
   const controls = useAnimation()
-  const isInView = useInView(ref as any, { margin: '-10% 0px', amount: 0.2, ...options })
+  const isInView = useInView(ref, { 
+    margin: '-10%',
+    amount: 0.2,
+    once: true,
+    ...options 
+  })
 
   useEffect(() => {
     if (isInView) controls.start({ opacity: 1, y: 0 })
